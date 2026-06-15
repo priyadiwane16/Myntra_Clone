2b7cd4a3694776eafd016faf44e46ea3d58985db
Priya <priyadiwane16@gmail.com>
2026-06-15 13:37:55 +0530

    Added new features and updated components

---
+++ 3-myntra-react-clone/src/components/Bag.jsx
@@ -0,0 +1,18 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Bag = () => {
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <div className="bag-page">
+          <div className="bag-items-container"></div>
+          <div className="bag-summary"></div>
+        </div>
+      </main>
+      <Footer></Footer>
+    </>
+  );
+};
+export default Bag;
